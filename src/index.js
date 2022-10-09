c7cff3035ffe20f512c4565b1e5bede64b1f92e4
korebhaumik <bhaumik.kore31@gmail.com>
2022-10-09 19:21:59 +0530

    korebhaumik/dnd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import App from "./app.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
